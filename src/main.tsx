4e626be3cf73ec694bf605555bd1800e3e1977fc
MrWindlike <MrWindlike@gmail.com>
2022-02-15 15:52:11 +0800

    style: add editor style

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import { initSunmaoUIEditor } from '@sunmao-ui/editor';
+import '@sunmao-ui/editor/dist/esm/index.css';
 import { EChartsLib } from './lib';
 
 const { Editor } = initSunmaoUIEditor({
